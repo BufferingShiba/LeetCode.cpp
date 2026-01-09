40d5de1a38400d88cd28a7b4b6e439a3c85a1ff5
cheekyshibe <acdoge.cao@gmail.com>
2026-01-09 20:48:48 +0800

    feat: update problem 865

--- src/leetcode/problems/smallest-subtree-with-all-the-deepest-nodes.cpp
+++ src/leetcode/problems/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -5,50 +5,40 @@ namespace problem_865 {
 
 // 辅助结构体，存储节点信息和深度
 struct NodeInfo {
-  TreeNode* node;    // 包含所有最深节点的最小子树根节点
-  int depth;         // 该子树的最大深度
-  
+  TreeNode* node;  // 包含所有最深节点的最小子树根节点
+  int depth;       // 该子树的最大深度
+
   NodeInfo(TreeNode* n, int d) : node(n), depth(d) {}
 };
-
-// 递归函数：返回当前节点的信息
-static NodeInfo dfs(TreeNode* root) {
-  if (!root) {
-    return NodeInfo(nullptr, 0);
-  }
-  
-  NodeInfo left_info = dfs(root->left);
-  NodeInfo right_info = dfs(root->right);
-  
-  // 如果左子树更深，返回左子树的结果
-  if (left_info.depth > right_info.depth) {
-    return NodeInfo(left_info.node, left_info.depth + 1);
-  }
-  
-  // 如果右子树更深，返回右子树的结果
-  if (right_info.depth > left_info.depth) {
-    return NodeInfo(right_info.node, right_info.depth + 1);
-  }
-  
-  // 左右子树深度相同，当前节点是最近公共祖先
-  return NodeInfo(root, left_info.depth + 1);
-}
 
 // 一次遍历递归解法
 // 时间复杂度: O(n)，每个节点访问一次
 // 空间复杂度: O(h)，递归栈深度，最坏情况 O(n)
 static TreeNode* solution1(TreeNode* root) {
+  function<NodeInfo(TreeNode*)> dfs = [&](TreeNode* node) -> NodeInfo {
+    if (!node) return NodeInfo(nullptr, 0);
+    NodeInfo left_info = dfs(node->left);
+    NodeInfo right_info = dfs(node->right);
+    if (left_info.depth > right_info.depth)
+      return NodeInfo(left_info.node, left_info.depth + 1);
+    if (right_info.depth > left_info.depth)
+      return NodeInfo(right_info.node, right_info.depth + 1);
+    return NodeInfo(node, left_info.depth + 1);
+  };
   return dfs(root).node;
 }
 
-SmallestSubtreeWithAllTheDeepestNodesSolution::SmallestSubtreeWithAllTheDeepestNodesSolution() {
+SmallestSubtreeWithAllTheDeepestNodesSolution::
+    SmallestSubtreeWithAllTheDeepestNodesSolution() {
   setMetaInfo({.id = 865,
                .title = "Smallest Subtree with all the Deepest Nodes",
-               .url = "https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/"});
+               .url = "https://leetcode.com/problems/"
+                      "smallest-subtree-with-all-the-deepest-nodes/"});
   registerStrategy("Recursive One-Pass", solution1);
 }
 
-TreeNode* SmallestSubtreeWithAllTheDeepestNodesSolution::subtreeWithAllDeepest(TreeNode* root) {
+TreeNode* SmallestSubtreeWithAllTheDeepestNodesSolution::subtreeWithAllDeepest(
+    TreeNode* root) {
   return getSolution()(root);
 }
 
